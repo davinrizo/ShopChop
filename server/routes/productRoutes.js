bcb0a5a968e11b712c4b469913b8c502763ec05f
davin <dizqar@gmail.com>
2021-05-24 23:22:32 +0700

    set up redux store, reducers, actions

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import asyncHandler from 'express-async-handler';
+import products from '../data/products.js';
 const router = express.Router();
 import Product from '../models/productModel.js';
 
@@ -16,8 +17,12 @@ router.get(
   asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    product ? res.json(product) : res.status(404);
-    throw new Error('Product not found!');
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404);
+      throw new Error('Product not found!');
+    }
   })
 );
 
