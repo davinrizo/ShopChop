8c5a50ba8c4510d50b70a39a6555fd2afb367d5d
davin <dizqar@gmail.com>
2021-05-28 23:59:56 +0700

    bugs fixed

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,6 +5,6 @@ import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
 router.post('/login', authUser);
-router.route('/profile').get((protect, getUserProfile));
+router.route('/profile').get(protect, getUserProfile);
 
 export default router;
